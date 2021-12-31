67c6aaa471fac1fcfe1fa4449e57da04ce13f273
EduardoLizama <74266905+EduardoLizama@users.noreply.github.com>
2021-12-31 13:51:26 -0300

    Update dbCreate.sql

--- dbCreate.sql
+++ dbCreate.sql
@@ -13,6 +13,8 @@ create table if not exists HABILIDAD
    
 create table if not exists VOL_HABILIDAD
     (ID_VOL_HABILIDAD int primary key,
+     ID_VOLUNTARIO int,
+     ID_HABILIDAD int,
      FOREIGN KEY (ID_VOLUNTARIO) REFERENCES VOLUNTARIO(ID_VOLUNTARIO),
      FOREIGN KEY (ID_HABILIDAD) REFERENCES HABILIDAD(ID_HABILIDAD));
    
@@ -25,10 +27,13 @@ create table if not exists EMERGENCIA
      REGION varchar(20),
      COMUNA varchar(20),
      CIUDAD varchar(20),
+     ID_INSTITUCION int,
      FOREIGN KEY (ID_INSTITUCION) REFERENCES INSTITUCION(ID_INSTITUCION));
    
 create table if not exists EME_HABILIDAD
     (ID_EME_HABILIDAD int primary key,
+     ID_EMERGENCIA int,
+     ID_HABILIDAD int,
      FOREIGN KEY (ID_EMERGENCIA) REFERENCES EMERGENCIA(ID_EMERGENCIA),
      FOREIGN KEY (ID_HABILIDAD) REFERENCES HABILIDAD(ID_HABILIDAD));
  
@@ -36,21 +41,27 @@ create table if not exists TAREA
     (ID_TAREA int primary key,
      NOMBRE_TAREA varchar(20),
      CAPACIDAD int,
+     ID_EMERGENCIA int,
      FOREIGN KEY (ID_EMERGENCIA) REFERENCES EMERGENCIA(ID_EMERGENCIA));
 
 create table if not exists TAREA_HABILIDAD
     (ID_TAREA_HABILIDAD int primary key,
+     ID_HABILIDAD int,
+     ID_TAREA int,
      FOREIGN KEY (ID_HABILIDAD) REFERENCES HABILIDAD(ID_HABILIDAD),
      FOREIGN KEY (ID_TAREA) REFERENCES TAREA(ID_TAREA)); 
     
 create table if not exists ESTADO_TAREA
     (ID_ESTADO_TAREA int primary key,
     NOMBRE varchar(20),
+    ID_TAREA int,
     FOREIGN KEY (ID_TAREA) REFERENCES TAREA(ID_TAREA));
     
 create table if not exists RANKING
     (ID_RANKING int primary key,
     NOMBRE varchar(20),
+    ID_VOLUNTARIO int,
+    ID_TAREA int,
     FOREIGN KEY (ID_VOLUNTARIO) REFERENCES VOLUNTARIO(ID_VOLUNTARIO),
     FOREIGN KEY (ID_TAREA) REFERENCES TAREA(ID_TAREA));
     
